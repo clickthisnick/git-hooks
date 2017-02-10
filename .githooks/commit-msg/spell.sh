71fd4e2f6aef703e56de58e0c4e8c5be052cf89c
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-09 21:56:34 -0500

    farewale theiir sir

---
+++ .githooks/commit-msg/spell.sh
@@ -0,0 +1,10 @@
+ASPELL=$(which aspell)
+if [ $? -ne 0 ]; then
+    echo "Aspell not installed - unable to check spelling" >&2
+    exit
+else
+    WORDS=$($ASPELL --mode=email --add-email-quote='#' list < "$1" | sort -u)
+fi
+if [ -n "$WORDS" ]; then
+    printf "\e[1;33m  Possible spelling errors found in commit message:\n\e[0m\e[0;31m%s\n\e[0m\e[1;33m  Use git commit --amend to change the message.\e[0m\n\n" "$WORDS" >&2
+fi
