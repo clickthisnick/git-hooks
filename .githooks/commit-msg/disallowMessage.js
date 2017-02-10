11d134becccbfdf5e285536c09addc4c755c9b1f
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-09 23:09:14 -0500

    foo

--- .githooks/commit-msg/disallowMessage.js
+++ .githooks/commit-msg/disallowMessage.js
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
 
 const exec = require('child_process').exec,
+    _ = require('lodash'),
     fs = require('fs');
 
 // Checking if any packages are missing
 return new Promise((resolve) => {
     fs.readFile('.git/COMMIT_EDITMSG', 'utf8', function(err, contents) {
-        console.log(contents);
         resolve(contents);
     });
 })
 // If packages are missing then run npm install
 .then((commitMessage) => {
+    console.log('here');
     console.log(commitMessage);
+    console.log(_.includes(['foo'], commitMessage));
+    if (_.includes(['foo'], commitMessage)) {
+        exec('exit 1', (error, stdout) => {
+            console.log(stdout); // eslint-disable-line no-console
+        });
+    }
 });
