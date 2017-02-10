3fae002358a06926838712fc3b6993429caed5e1
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-10 10:14:44 -0500

    adding feat to list of prefixs

---
+++ .githooks/prepush/checkBranchName.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const util = require('./../util.js'),
+  _ = require('lodash'),
+  requiredBranchPrefixs = ['chore', 'hotfix', 'bugfix', 'feat'],
+  ignoreBranches = ['master', 'latest'];
+
+return util.getBranchName()
+  .then((branchName) => {
+      let isPrefixed = false;
+
+      // Don't give error for branches we don't want to be warned on
+      if (_.includes(ignoreBranches, branchName)) {
+          return;
+      }
+
+      // Figure out if branch is prefixed
+      _.forEach(requiredBranchPrefixs, (prefix) => {
+          if (_.startsWith(branchName, prefix)) {
+            isPrefixed = true;
+          }
+      })
+
+      if (!isPrefixed) {
+          console.log('\n\n');
+          console.log('\t\x1b[103m', 'Branch name missing required prefix' ,'\x1b[0m');
+          console.log('\t\x1b[103m', 'Rename branch with one of the following commands:' ,'\x1b[0m');
+          _.forEach(requiredBranchPrefixs, (prefix) => {
+                console.log(`\tgit branch -m ${branchName} ${prefix}-${branchName}`)
+          })
+          console.log('\n\n');
+
+      }
+  });
