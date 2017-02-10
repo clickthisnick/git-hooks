4c9855a22f1ccee532d8e011297d19b769113287
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-10 09:25:37 -0500

    this is so legit

---
+++ .githooks/commit-msg/1stripMultipleSpaces.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const
+
+    // Libraries
+    exec = require('child_process').exec,
+    _ = require('lodash'),
+    fs = require('fs');
+
+// Checking if any packages are missing
+return new Promise((resolve) => {
+    fs.readFile('.git/COMMIT_EDITMSG', 'utf8', function(err, commitMessage) {
+        resolve(commitMessage);
+    });
+})
+// If packages are missing then run npm install
+.then((commitMessage) => {
+    // Remove multiple spaces
+    commitMessage = commitMessage.replace(/  +/g, ' ');
+    fs.writeFile('.git/COMMIT_EDITMSG', `${commitMessage}`, function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
