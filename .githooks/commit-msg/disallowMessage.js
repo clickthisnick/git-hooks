b066b9dddd94d307c5fe2b10f15500ea5c43596a
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-09 22:48:08 -0500

    frog

---
+++ .githooks/commit-msg/disallowMessage.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const exec = require('child_process').exec;
+
+// Checking if any packages are missing
+return Promise.resolve()
+// If packages are missing then run npm install
+.then(() => {
+    exec('$(cat "$1")', (error, stdout) => {
+        console.log(stdout); // eslint-disable-line no-console
+    });
+});
