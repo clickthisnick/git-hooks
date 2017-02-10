72dae9645a6d20380501835946656f03b4b6d35d
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-09 23:50:56 -0500

    Adding more githooks

--- .githooks/commit-msg/disallowMessage.js
+++ .githooks/commit-msg/disallowMessage.js
@@ -21,15 +21,17 @@ return new Promise((resolve) => {
 .then((commitMessage) => {
     const wordCount = commitMessage.split(' ').length;
     let errorString = [];
-    console.log(wordCount);
+
     if (wordCount < minimumWordCount) {
         errorString.push(`Commit Message: "${commitMessage}" Not Descriptive. Please use at least ${minimumWordCount} words`);
     }
 
     if (errorString.length > 0) {
         // http://misc.flogisoft.com/bash/tip_colors_and_formatting
-        console.log('\x1b[103m', _.join(errorString, '\n') ,'\x1b[0m');
-        console.log('\x1b[31m', 'Aborting Commit.' ,'\x1b[0m');
+        console.log('\n\n');
+        console.log('\t\x1b[103m', _.join(errorString, '\n') ,'\x1b[0m');
+        console.log('\t\x1b[31m', 'Aborting Commit.' ,'\x1b[0m');
+        console.log('\n\n');
         process.exit(1);
     }
 });
