2ce10856d8d45e5b4420423d31792e2ebda01a15
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-11 13:35:32 -0500

    Bugfix - Ignoring githooks word

--- .githooks/commit-msg/3spell.js
+++ .githooks/commit-msg/3spell.js
@@ -17,7 +17,7 @@ return util.getCommitMessage()
         } else {
             return util.bash(`${aspell.output} list < .git/COMMIT_EDITMSG | sort -u`)
                 .then((res) => {
-                    const ignoredWords = ['bugfix', 'githook'];
+                    const ignoredWords = ['bugfix', 'githooks'];
 
                     let misspelledWords = _(res.output)
                         .split('\n')
