c356191f8aae0aa234f99ffd491299d4833875a2
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-11 11:59:36 -0500

    Bugfix - Testing this oute

--- .githooks/commit-msg/3spell.sh
+++ .githooks/commit-msg/3spell.sh
@@ -5,7 +5,7 @@ if [ $? -ne 0 ]; then
     exit
 else
     WORDS=$($ASPELL --mode=email --add-email-quote='#' list < "$1" | sort -u)
-    echo ${WORDS}
+    echo ${WORDS} 
 fi
 if [ -n "$WORDS" ]; then
     printf "\e[1;33m  Possible spelling errors found in commit message:\n\e[0m\e[0;31m%s\n\e[0m\e[1;33m  Use git commit --amend to change the message.\e[0m\n\n" "$WORDS" >&2
