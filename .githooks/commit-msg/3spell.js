8e9106b78d8475aef781ad0c475de83b6f86fa7d
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-11 14:13:44 -0500

    Chore - Bugfix for the fixer uppers

--- .githooks/commit-msg/3spell.js
+++ .githooks/commit-msg/3spell.js
@@ -25,7 +25,8 @@ return util.getCommitMessage()
                     }
 
                     // Adding s to the words and also ignoring them
-                    const ignoredWordList = _.concat(constants.IGNORED_MISSPELLINGS, _.map(ignoredWords, (word) => `${word}s`)),
+                    const ignoredWordsWithS = _.map(constants.IGNORED_MISSPELLINGS, (word) => `${word}s`);
+                        ignoredWordList = _.concat(constants.IGNORED_MISSPELLINGS, ignoredWordsWithS);
                         misspelledWords = _(res.output)
                             .split('\n')
                             .map((word) => _.lowerCase(word))
