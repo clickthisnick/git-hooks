4ac02d4720c668f200bb31d3a7bc366d30d0a18c
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-11 12:06:25 -0500

    Bugfix - Frag went to thea storea and hert hiz leg

--- .githooks/commit-msg/3spell.sh
+++ .githooks/commit-msg/3spell.sh
@@ -5,19 +5,19 @@ if [ $? -ne 0 ]; then
     exit
 else
     # Words found that are thought to be mistakes
-    SPELLING_MISTAKES=$($ASPELL --mode=email --add-email-quote='#' list < "$1" | sort -u)
-    echo ${SPELLING_MISTAKES}
-    
+    misspelled_words=$($ASPELL --mode=email --add-email-quote='#' list < "$1" | sort -u)
+    echo ${misspelled_words}
+
     # Words that we don't want to alert user are mistakes
-    IGNORE_MISTAKES=(Bugfix bugfix)
+    ignored_words=(Bugfix bugfix)
 
     # Filtering out the ignored mistakes
-    for i in "${IGNORE_MISTAKES[@]}"; do
-        SPELLING_MISTAKES=(${SPELLING_MISTAKES[@]//*$i*})
+    for i in "${ignored_words[@]}"; do
+        misspelled_words=(${misspelled_words[@]//*$i*})
     done
 
-    echo ${SPELLING_MISTAKES}
+    echo ${misspelled_words}
 fi
-if [ -n "$SPELLING_MISTAKES" ]; then
-    printf "\e[1;33m  Possible spelling errors found in commit message:\n\e[0m\e[0;31m%s\n\e[0m\e[1;33m  Use git commit --amend to change the message.\e[0m\n\n" "$SPELLING_MISTAKES" >&2
+if [ -n "$misspelled_words" ]; then
+    printf "\e[1;33m  Possible spelling errors found in commit message:\n\e[0m\e[0;31m%s\n\e[0m\e[1;33m  Use git commit --amend to change the message.\e[0m\n\n" "$misspelled_words" >&2
 fi
